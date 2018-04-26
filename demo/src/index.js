f0ed6dbf0be50e6c362d63d33178e0ca56f408a5
Tarik Huber <huber.tarik@gmail.com>
2018-04-26 16:14:23 +0200

    Installed date picker and need to migrate superselectfield

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,10 +1,12 @@
-import {render} from 'react-dom'
+import { render } from 'react-dom'
 import React from 'react'
 import App from './App.js'
 import { Provider } from 'react-redux'
 import reducers from './reducers'
 import { createLogger } from 'redux-logger'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import MuiPickersUtilsProvider from 'material-ui-pickers/utils/MuiPickersUtilsProvider'
+import MomentUtils from 'material-ui-pickers/utils/moment-utils'
 import { createStore, compose, applyMiddleware } from 'redux'
 
 const logger = createLogger({})
@@ -13,8 +15,10 @@ const store = createStore(reducers, {}, compose(applyMiddleware(logger)))
 
 render(
   <Provider store={store}>
-    <MuiThemeProvider>
-      <App />
-    </MuiThemeProvider>
+    <MuiPickersUtilsProvider utils={MomentUtils}>
+      <MuiThemeProvider>
+        <App />
+      </MuiThemeProvider>
+    </MuiPickersUtilsProvider>
   </Provider>
   , document.querySelector('#demo'))
